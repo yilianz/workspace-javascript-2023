a7054217b1a83109ec70e916b65d5744f8ea7010
yilianz <yilianz@gmail.com>
2020-10-07 13:23:53 -0400

    1 file modified, 1 added

---
+++ js/hello.js
@@ -0,0 +1,20 @@
+
+/* Send a customized message based on time
+*/
+console.log("I am in the hello.js");
+
+// get the date and time 
+var today = new Date();
+var hoursNow = today.getHours();
+
+// display the greeting message 
+var greetings = "";
+if (hoursNow <= 12){
+  greetings = "Good Morning";
+}else{
+  greetings = "Good Afternoon";
+}
+
+
+// write to the document 
+document.write(greetings);
