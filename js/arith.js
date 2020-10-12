10e90cd4c7918bcb000a69237e24362a8d086926
yilianz <yilianz@gmail.com>
2020-10-12 13:13:21 -0400

    1 file modified, 1 added

---
+++ js/arith.js
@@ -0,0 +1,12 @@
+// get the element 
+
+// element --- property
+var num1 = parseInt(document.getElementById('n1').textContent); 
+var num2 = parseInt(document.getElementById('n2').textContent); 
+
+
+//process
+var c = num1 + num2;
+
+//display
+document.write(c);
